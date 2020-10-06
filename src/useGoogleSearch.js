57268614514f13906f8921490594e637a1b6fa64
im-ankitjais <ankitmyself2017@gmail.com>
2020-10-06 21:04:26 +0530

    key moved to env file

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,14 +1,12 @@
 import { useState, useEffect } from 'react';
 
-const CONTEXT_KEY = "e9e3e0fdde0f32093";
-
 const useGoogleSearch = (term) => {
     const [data, setData] = useState(null);
     useEffect(() => {
         const fetchData = async() => {
             // use axios instead of fetch
             fetch(
-                `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+                `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_API_KEY}&cx=${process.env.REACT_APP_CONTEXT_KEY}&q=${term}`
             )
             .then(response => response.json())
             .then(result => {
